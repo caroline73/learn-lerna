39fd00d3fb179d4aa4bb3938bf7addaf9034c4bc
shaohui.zhang <shaohui.zhang@yunzhanghu.com>
2020-08-26 16:09:43 +0800

    feat: firstcommit

--- packages/testing-lerna-popular/index.js
+++
@@ -1,9 +0,0 @@
-const { API } = require('testing-lerna-core');
-const axios = require('axios');
-
-const getPopularImg = () => axios.get(API);
-
-module.exports = getPopularImg;
-
-// 测试代码，发布时删除
-// getPopularImg().then((res) => console.log(res.data.length));
